5d32426bfcb58d7e7c50d781fcb665bc3f1cd582
alanmulero <alan.mulero@gmail.com>
2024-12-02 13:18:40 -0300

    Generics e lambda implementados

---
+++ scrennMatch/src/test/java/br/com/screeMatch/scrennMatch/ScrennMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.screeMatch.scrennMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrennMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
